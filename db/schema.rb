6831f3623b704003a7f979760ea380444a71e35c
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-14 22:27:00 +0900

    usersテーブルに認証用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_14_123701) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_14_132258) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_14_123701) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activation_digest", null: false
+    t.boolean "activated", default: false, null: false
+    t.datetime "activated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
