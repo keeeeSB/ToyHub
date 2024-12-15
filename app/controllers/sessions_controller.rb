6d77c4af92e407be1f8ea33397b2069365d1cdbb
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-15 15:57:52 +0900

    Sessionsコントローラーにアクションを追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      if user.activated?
+        reser_sessin
+        log_in user
+        flash[:success] = "ログインしました。"
+        redirect_to User
+      else
+        flash[:warning] = "アカウントが承認されていません。メールを確認してください。"
+        redirect_to root_path
+      end
+    else
+      flash.now[:danger] = "メールアドレス、又はパスワードが間違っています。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_path, status: :see_other
+  end
+end
