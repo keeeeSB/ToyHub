30e22d2500046374461a77f23562aa4b18652b3d
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-15 15:30:26 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/signup",  to: "users#new"
   post "/signup", to: "users#create"
   resources :users, only: [:show]
+  resources :account_activations, only: [:edit]
 end
