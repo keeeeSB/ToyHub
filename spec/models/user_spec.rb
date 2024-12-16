b9ed8e6052d7768f3293ed2f6a79eb6a6179a93b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-16 17:30:15 +0900

    Userモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "バリデーションのテスト" do
+    it "有効なユーザーを作成できる" do
+      expect(user).to be_valid
+    end
+
+    it "名前は必須である" do
+      user.name = " "
+      expect(user).to_not be_valid
+    end
+
+    it "メールアドレスは必須である" do
+      user.email = " "
+      expect(user).to_not be_valid
+    end
+
+    it "パスワードは必須である" do
+      user.password = " "
+      expect(user).to_not be_valid
+    end
+
+    it "パスワードは最低６文字以上である" do
+      user.password = "hello"
+      user.password_confirmation = "hello"
+      expect(user).to_not be_valid
+
+      user.password = "goodbye"
+      user.password_confirmation = "goodbye"
+      expect(user).to be_valid
+    end
+  end
 end
