0016c9c765ea5e2e1c621743a0ecf41bf03d3ccd
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-20 09:38:22 +0900

    Taskモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_18_191839) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_20_003535) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "toys", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
