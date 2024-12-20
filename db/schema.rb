7fbd7ed3eb2e52ac4724a994efd2bb5a3b8b22cc
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-20 09:40:27 +0900

    Tagモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_20_003535) do
-  create_table "tasks", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2024_12_20_003947) do
+  create_table "tags", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
