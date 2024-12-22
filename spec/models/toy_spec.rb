2c04974c21375eab245e4e26fad13dbb675da623
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-22 20:57:40 +0900

    タグ登録のテストを追加

--- spec/models/toy_spec.rb
+++ spec/models/toy_spec.rb
@@ -23,4 +23,21 @@ RSpec.describe Toy, type: :model do
       expect(toy).to_not be_valid
     end
   end
+
+  describe "#assign_tags" do
+    it "半角スペースで区切られたタグを保存する" do
+      toy.assign_tags("スポーツ 球技")
+      expect(toy.tags.map(&:name)).to contain_exactly("スポーツ", "球技")
+    end
+
+    it "全角スペースで区切られたタグを保存する" do
+      toy.assign_tags("スポーツ　球技")
+      expect(toy.tags.map(&:name)).to contain_exactly("スポーツ", "球技")
+    end
+
+    it "半角と全角スペースが混同していても、正しく分割して保存する" do
+      toy.assign_tags("スポーツ 球技　アウトドア")
+      expect(toy.tags.map(&:name)).to contain_exactly("スポーツ", "球技", "アウトドア")
+    end
+  end
 end
